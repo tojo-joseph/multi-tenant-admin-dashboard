6d4c1faeb3055d7de40c82256e4f4bdaf69e0c28
tojo-joseph <tojo.joseph18@gmail.com>
2025-02-21 17:59:51 +0530

    new login and backend changes

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
+  username: string = '';
+  password: string = '';
 
+  @Output() signupFormSubmitted = new EventEmitter<{
+    username: string;
+    password: string;
+  }>();
+
+  submitForm() {
+    console.log('Username', this.username, this.password);
+    this.signupFormSubmitted.emit({
+      username: this.username,
+      password: this.password,
+    });
+  }
 }
